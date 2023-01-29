eb224028585a2414f782bc36823b763ce46991e4
Viacheslav Malyi <viacheslavm@leadsmaket.com>
2023-01-29 14:39:03 +0200

    s

--- two-sum/two-sum/Program.cs
+++ two-sum/two-sum/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+var s = new Solution();
+var r = s.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+Console.WriteLine(r);
+
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = 0; j < nums.Length; j++)
+            {
+                if (i == j)
+                    continue;
+
+                if (nums[i] + nums[j] == target)
+                    return new[] { i, j };
+            }
+        }
+
+        return null;
+    }
+}
