bd9c807103eaff21bd970a7550949cb9fb27f8c9
Viacheslav Malyi <viacheslavm@leadsmaket.com>
2023-01-29 20:44:56 +0200

    search-a-2d-matrix

---
+++ search-a-2d-matrix/search-a-2d-matrix/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        if (matrix.Length == 0 || matrix[0].Length == 0)
+            return false;
+        int end;
+        int mid;
+        int r = matrix.Length;
+        int c = matrix[0].Length;
+        for (int i = 0; i < r; i++)
+        {
+            if (matrix[i][0] <= target && matrix[i][c - 1] >= target)
+            {
+                var start = 0;
+                end = c - 1;
+                while (start <= end)
+                {
+                    mid = (start + end) / 2;
+                    if (target == matrix[i][mid])
+                        return true;
+                    else if (target < matrix[i][mid])
+                        end = mid - 1;
+                    else
+                        start = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+}
