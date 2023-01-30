823e5335bcd3affb9ccaa9db155a9c979598c952
Viacheslav Malyi <viacheslavm@leadsmaket.com>
2023-01-30 20:23:45 +0200

    longest_substring_without_repeating_characters

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+var s = new Solution();
+
+var str6 = "dvdf";
+Console.WriteLine(s.LengthOfLongestSubstring(str6));
+
+var str5 = "au";
+Console.WriteLine(s.LengthOfLongestSubstring(str5));
+
+var str0 = "a";
+Console.WriteLine(s.LengthOfLongestSubstring(str0));
+
+var str1 = "abcabcbb";
+Console.WriteLine(s.LengthOfLongestSubstring(str1));
+
+var str2 = "bbbbb";
+Console.WriteLine(s.LengthOfLongestSubstring(str2));
+
+var str3 = "pwwkew";
+Console.WriteLine(s.LengthOfLongestSubstring(str3));
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (s == null || s.Length == 0) return 0;
+
+        int max = 0;
+        var n = s.Length;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                var current = j - i + 1;
+                if (check(s, i, j))
+                    if (max < current)
+                        max = current;
+            }
+        }
+
+        return max;
+    }
+
+    private bool check(string s, int start, int end)
+    {
+        HashSet<char> chars = new HashSet<char>();
+
+        for (int i = start; i <= end; i++)
+        {
+            if (chars.Contains(s[i]))
+                return false;
+
+            chars.Add(s[i]);
+        }
+
+        return true;
+    }
+}
