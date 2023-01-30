38c9c33bce1e295d9dd01823db984fa0b3335d04
Viacheslav Malyi <viacheslavm@leadsmaket.com>
2023-01-30 10:06:31 +0200

    Update Program.cs

--- palindrome-number/palindrome-number/Program.cs
+++ palindrome-number/palindrome-number/Program.cs
@@ -1,12 +1,14 @@
 ﻿var s = new Solution();
 Console.WriteLine(s.IsPalindrome(125621));
+Console.WriteLine(s.IsPalindrome(12521));
+Console.WriteLine(s.IsPalindrome(1221));
 public class Solution
 {
     public bool IsPalindrome(int x)
     {
         if (x < 0 ) return false;
 
-        var digits = x.ToString().Select(t => int.Parse(t.ToString())).ToArray();
+        var digits = x.ToString();
         var isPalindrome = true;
         var len = digits.Length / 2;
         for (var i = 0; i < len; i++)
