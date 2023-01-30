0f2481595b1b1242aab3a1634ed21eb2ebbb6e1f
Viacheslav Malyi <viacheslavm@leadsmaket.com>
2023-01-30 09:55:57 +0200

    roman-to-integer

---
+++ roman-to-integer/roman-to-integer/Program.cs
@@ -0,0 +1,43 @@
+﻿var s = new Solution();
+
+Console.WriteLine(s.RomanToInt("III"));
+Console.WriteLine(s.RomanToInt("IV"));
+Console.WriteLine(s.RomanToInt("XII"));
+Console.WriteLine(s.RomanToInt("XXVII"));
+Console.WriteLine(s.RomanToInt("LVIII"));
+Console.WriteLine(s.RomanToInt("MCMXCIV"));
+
+
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        var sum = 0;
+        var prev = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            var current = ResolveSymbol(s[i]);
+            sum += current;
+            if (prev < current)
+                sum -= prev*2;
+            prev = current;
+        }
+
+        return sum;
+    }
+
+    private static int ResolveSymbol(char c)
+    {
+        return c switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => throw new ArgumentOutOfRangeException(nameof(c), c, null)
+        };
+    }
+}
